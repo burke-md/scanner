283309bceecd69a073363e4e65e48fc6e0aaaa0d
MB <michael.dburke56@gmail.com>
2022-10-30 13:07:53 -0500

    Handle snowsight payments within script.

--- snowsight_scanner.py
+++ snowsight_scanner.py
@@ -16,10 +16,32 @@ def pay_sync():
     Submit payment to chainsight
     """
 
-    snowsight_contract = brownie.Contract.from_explorer(SNOWSIGHT_CONTRACT_ADDRESS)
+    snowsight_contract = 
+brownie.Contract.from_explorer(SNOWSIGHT_CONTRACT_ADDRESS)
+    
+    block_payment = snowsight_contract.paymentPerBlock() * (
+        brownie.chain.height
+        + snowsight_contract.maximumPaymentBlocks()
+        - snowsight_contract.payments(degenbot.address)[-1]
+    )
+
+    snowsight_contract.pay(
+        {
+            "from": degenbot,
+            "value": min(
+                block_payment,
+                snowsight_contract.CalculateMaxPayment(),
+            ),
+            "priority_fee": 0,
+        }
+    )
 
 
 async def watch_pending_transactions():
+
+    signed_message = degenbot.sign_defunct_message(
+        "Sign this message to authenticate your wallet with Snowsight."
+    )
 
 
 async def main():
