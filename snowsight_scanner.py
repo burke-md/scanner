d872249f21421c6ad5b02c6463bc6c38de4e2262
MB <michael.dburke56@gmail.com>
2022-10-30 17:49:08 -0500

    Observe websocket stream and filter out txs as required.

--- snowsight_scanner.py
+++ snowsight_scanner.py
@@ -42,6 +42,60 @@ async def watch_pending_transactions():
     signed_message = degenbot.sign_defunct_message(
         "Sign this message to authenticate your wallet with Snowsight."
     )
+    
+    async for websocket in websockets.connect(
+        uri="ws://avax.chainsight.dev:8595",
+        ping_timeout=none,
+    ):
+
+        try:
+            await websocket.send(
+                json.dumps({"signed_key":
+signed_message.signature.hex()})
+            )
+            resp = json.loads(await websocket.recv())
+            print(resp)
+
+            # If currently unathorized, make payment & loop
+            if "unauthenticated" in resp["status"]:
+                pay_sync()
+                continue 
+
+            if resp["status"] == "authenticated":
+
+                while True:
+
+                    tx_message = json.loads(
+                        await asyncio.wait_for(
+                            websocket.recv(),
+                            timeout=30,
+                        )
+                    )
+
+                    tx_to = tx_message["to"].lower()
+                    # Filter for only desiered transactions
+                    if tx_to not in [address.lower() for address in 
+                                     ROUTERS.keys()]:
+                        continue
+                    else:
+                        func, params = brownie.web3.eth.contract(
+
+address=brownie.web3.toChecksumAddress(tx_to),
+                            abi=ROUTERS[tx_to]["abi"],
+                        ).decode_function_input(tx_message["input"])
+
+                    # Show tx w/ 'path' arg
+                    if params.get("path") in [
+                        [cra.address, wavax.address],
+                        [wavax.address, cra.address],
+                    ]:
+
+                        print()
+                        print("=== Pending WAVX-CRA swap ===")
+                        print(func.fn_name)
+                        print(params)
+
+
 
 
 async def main():
